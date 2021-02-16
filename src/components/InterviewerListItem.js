475a821264a8e6dbe5d4c4f740e33160b25718d0
Cass Bredeson <c.s.bredeson@gmail.com>
2021-02-16 15:50:05 -0700

    add Appointment components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,10 +2,10 @@ import React from "react";
 import "components/InterviewerListItem.scss";
 import classnames from "classnames";
 
-export default function InterviewerListItem (props) {
-  const InterviewerClass=classnames("interviewers__item", {"interviewers__item--selected": props.selected});
+export default function InterviewerListItem(props) {
+  const interviewerClass = classnames("interviewers__item", {"interviewers__item--selected": props.selected});
 return (
-<li className={InterviewerClass} onClick={() => props.setInterviewer(props.name)}>
+<li className={interviewerClass} onClick={props.setInterviewer}>
   <img
     className="interviewers__item-image"
     src={props.avatar}
--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,14 @@ import DayListItem from "components/DayListItem";
 import Button from "components/Button";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -94,7 +102,7 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={event => action("setInterviewer")(interviewer.id)}
     />
   ));
 
@@ -123,3 +131,16 @@ storiesOf("InterviewerList", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+  .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+  .add("Status", () => <Status message="Deleting"/>)
+  .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
