d95fb685ac446f9f9d25c591959051a0f10b741b
Cass Bredeson <c.s.bredeson@gmail.com>
2021-02-19 16:59:09 -0700

    add saving interview view

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,12 +4,15 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "../../hooks/useVisualMode";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
+
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
@@ -18,6 +21,8 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
+
+    transition(SAVING);
     props.bookInterview(props.id, interview)
       .then(() => transition(SHOW));
   }
@@ -33,6 +38,9 @@ export default function Appointment(props) {
       )}
       {mode === CREATE && (
         <Form interviewers={props.interviewers} onCancel={back} onSave={save} />
+      )}
+      {mode === SAVING && (
+        <Status message="Saving"/>
       )}
     </article>
   )
